6afb8c393d49f1d3fd3fda68facf28d780484e33
lilingyun01 <lilingyun01@qianxin.com>
2021-06-08 14:07:26 +0800

    创建代码

---
+++ pages/index/loginPage.py
@@ -0,0 +1,41 @@
+'''
+    统一登陆界面
+
+'''
+from common import basePage
+from selenium import webdriver
+
+
+class LoginPage(basePage.BasePage):
+
+    '''统一登录网址'''
+    url="http://10.95.152.140:8080/"
+
+    '''
+        系统相关元素
+    '''
+    #button=('xpath','/html/body/div[10]/div[2]/div[3]/ul/li[1]')
+    username=('id',"username")
+    password=('id','password')
+    login_button=('//*[@id="login"]')
+
+    '''
+        系统相关组件
+    '''
+    def crm_login(self,username="secadmin",password="111111111a!"):
+        '''登陆系统'''
+        self.get(self.url)
+        #self.click(self.crm_button)
+        self.sendKeys(self.username,text=username)
+        self.sendKeys(self.password,text=password)
+        self.click(self.login_button)
+
+
+
+if __name__ == '__main__':
+
+    driver=webdriver.Chrome()
+    login_page=LoginPage(driver)
+    login_page.crm_login()
+
+
---
+++ testcase/crm/test_login.py
@@ -0,0 +1,39 @@
+import pytest
+from pages.index import loginPage
+from selenium import webdriver
+#from allure import MASTER_HELPER
+import allure
+
+
+
+
+@allure.feature("登陆")
+class TestLogin():
+
+    @allure.step("初始化启动浏览器")
+    def setup_class(self):
+        '''用例执行前，启动浏览器，创建chrome实例'''
+        driver=webdriver.Chrome()
+        self.login_page=loginPage.LoginPage(driver)
+
+    @allure.step("关闭浏览器")
+    def teardown_class(self):
+        '''用例执行完毕，关闭浏览器'''
+        self.login_page.quit()
+
+    @allure.testcase("用例名：登陆——正常场景")
+    def test_login_001(self):
+        with allure.step("登陆"):
+            self.login_page.crm_login()
+
+
+
+    @allure.testcase("用例名：登陆——账号错误")
+    def test_login_002(self):
+        self.login_page.crm_login(username="200",password="123456")
+        self.login_page.get_screen()
+
+if __name__ == '__main__':
+    pytest.main(['-s','test_login.py'])
+
+    
